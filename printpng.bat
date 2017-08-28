f6801d9f1b3d2aa35b26c8325c53b76d241ccf3e
djinnsour <djinnsour@users.noreply.github.com>
2017-08-28 14:35:14 -0500

    Change to mspaint
    Added more comments, fixed executable name

--- printpng.bat
+++ printpng.bat
@@ -1,14 +1,19 @@
 @echo off
 REM Assume files are in c:\printme, c:\printme\working, c:\printme\printed exist
+REM Create your file and copy/save it to c:\printme. Every time the script runs
+REM it will check for PNG files in that directory. If it finds them it will move
+REM them to a working directory, print them, and move them to a finished directory.
 REM 
 REM Might want to try http://www.robvanderwoude.com/printfiles.php#PrintAny also
 REM 
 REM Moves the png file to another directory for processing
 move c:\printme\*.png c:\printme\working
 REM 
-REM Processes each file in c:\printme\working, printing the file using paint
+REM Processes each file in c:\printme\working, printing the file using Microsoft
+REM Paint to the default printer. If you want to send it to a specific printer
+REM change the command to: mspaint.exe /pt printername "%%~ff"
 for /f "tokens=*" %%f in ('dir /od /tw /b /a-d "C:\printme\working\*.*"') do (
- paint.exe /p "%%~ff"
+ mspaint.exe /p "%%~ff"
  )
  REM 
  REM Once the files are printed it moves them to another directory

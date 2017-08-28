87909bd3a25f00753e3174b45a5cb2ab2c00e18f
djinnsour <djinnsour@users.noreply.github.com>
2017-08-28 14:28:30 -0500

    Fixed comment lines
    REM not #

--- printpng.bat
+++ printpng.bat
@@ -1,5 +1,7 @@
 @echo off
-#Assume files are in c:\printme, c:\printme\working, c:\printme\printed exist
+REM Assume files are in c:\printme, c:\printme\working, c:\printme\printed exist
+REM 
+REM Might want to try http://www.robvanderwoude.com/printfiles.php#PrintAny also
 move c:\printme\*.png c:\printme\working
 for /f "tokens=*" %%f in ('dir /od /tw /b /a-d "C:\printme\working\*.*"') do (
  paint.exe /p "%%~ff"
